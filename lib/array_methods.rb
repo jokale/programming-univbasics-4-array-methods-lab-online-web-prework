b6ee411bf05fbbbad29aa486ad0af7934b44cdf2
Joanna E K <kalemajoanna@gmail.com>
2019-10-29 18:50:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,9 @@
 def using_include(array, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.include?("wow")
+  element1 = "wow"
+    element2 = "Tardis"
+  array.include?(element1)
+  array.include?(element2)
   
 end
 
