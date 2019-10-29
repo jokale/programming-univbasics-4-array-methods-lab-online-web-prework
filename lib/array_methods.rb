feca2ff2536243e5f44a2287218e8489d9a9ba1c
Joanna E K <kalemajoanna@gmail.com>
2019-10-29 18:58:18 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,6 +3,7 @@ def using_include(array, element)
   array.include?("wow")
     array.include?("Tardis")
   end 
+end
 
 def using_sort(array)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
